574c2d90aaa8bb423aa983aafd8e92b33912d084
moutasdimitris <32466021+moutasdimitris@users.noreply.github.com>
2020-01-28 23:02:14 +0200

    Initial Commit

---
+++ searchEngine/searchEngine/src/com/company/TFD.java
@@ -0,0 +1,37 @@
+package com.company;
+
+ class TFD {
+     public void setText(String text) {
+         this.text = text;
+     }
+
+     public String getText() {
+         return text;
+     }
+
+     String text;
+
+     public String getDoc_id() {
+         return doc_id;
+     }
+
+     public void setDoc_id(String doc_id) {
+         this.doc_id = doc_id;
+     }
+
+     String doc_id;
+
+     public Integer getFreq() {
+         return freq;
+     }
+
+     public void setFreq(Integer freq) {
+         this.freq = freq;
+     }
+
+     Integer freq;
+
+
+
+
+}
